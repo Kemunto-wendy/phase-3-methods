6f546eb00129c6452aa970a3335dc533071d583c
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-08-22 23:38:25 +0300

    changes

--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+name = "Naureen"
+
+def greet name
+   puts "Hello, #{name}"
+end
+greet("Naureen")
+greet("Jimmy")
+
+#adds two numbers
+def add x,y
+  x + y
+end
+add(2, 5)
+
+#half
+def halve(a,b)
+end
+
+def greet name
+  puts "Hello, #{name}!"
+end
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default name="programmer"
+  puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def halve (num)
+  if num.class != Integer
+  return nil
+  end
+  num/2
+end
+halve("k")
